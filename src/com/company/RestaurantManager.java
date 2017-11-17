fdfa23d2280e8513a80ac10839ce0ea78e23266d
Thanapoom Rattanathumawat <thanapoom.ta@ku.th>
2017-11-17 17:50:24 +0700

    Finished

--- src/com/company/RestaurantManager.java
+++ src/com/company/RestaurantManager.java
@@ -4,16 +4,20 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
+
+/*
+Set menu file as RELATIVE PATH e.g: "data/menu.txt"
+ */
 
 public class RestaurantManager {
 
+    public static final String MENU_FILE = "data/menu.txt";
     private static String[] menuItems;
 
     private static double[] prices;
 
-    public static String[] readFileName() {
-        String path = "C:\\Users\\Poom\\Desktop\\menu.txt";
+    public static String[] getMenuItems() {
+        String path = MENU_FILE;
         File file = new File(path);
         try {//Sometimes if a program runs into an error, we provide an exit for the program.
             BufferedReader takeAllLine = new BufferedReader(new FileReader(file));//Put everything that is read in the file and put in it BufferedReader
@@ -26,6 +30,7 @@ public class RestaurantManager {
             while ((lineString = takeAllLine.readLine()) != null) {
                 lines += 1;
             }
+
             takeAllLine = new BufferedReader(new FileReader(file));
             prices = new double[lines];
             menuItems = new String[lines];
@@ -41,12 +46,14 @@ public class RestaurantManager {
             }
             takeAllLine.close();
         } catch (IOException e) {
+            System.out.println("There is an error.");
             e.printStackTrace();
         }
         return menuItems;
     }
-    public static double[] readFilePrice() {
-        String path = "C:\\Users\\Poom\\Desktop\\menu.txt";
+
+    public static double[] getPrices() {
+        String path = MENU_FILE;
         File file = new File(path);
         try {//Sometimes if a program runs into an error, we provide an exit for the program.
             BufferedReader takeAllLine = new BufferedReader(new FileReader(file));//Put everything that is read in the file and put in it BufferedReader
@@ -59,6 +66,7 @@ public class RestaurantManager {
             while ((lineString = takeAllLine.readLine()) != null) {
                 lines += 1;
             }
+
             takeAllLine = new BufferedReader(new FileReader(file));
             prices = new double[lines];
             menuItems = new String[lines];
@@ -70,10 +78,10 @@ public class RestaurantManager {
                 String part2 = parts[1].trim();//e.g round 1 = 45
                 prices[count] = Double.parseDouble(part2);//change from string to double.
                 count++;
-
             }
             takeAllLine.close();
         } catch (IOException e) {
+            System.out.println("There is an error.");
             e.printStackTrace();
         }
         return prices;
--- src/com/company/Skerestaurant.java
+++ src/com/company/Skerestaurant.java
@@ -1,30 +1,28 @@
 package com.company;
 
-import java.util.*;
+import java.util.*; // import everything from the java.util package.
 
 public class Skerestaurant {
 
     public static Scanner scan = new Scanner(System.in);
 
-    public static int choice = 0;
+    public static String[] menuItems;
 
-    public static String[] getMenuItems;
-
-    public static double[] getPrices;
+    public static double[] prices;
 
     public static int[] orders;
 
     public static void menuTable(){
             double total = 0;
             System.out.println("+------ Menu -------+--- Qty ---+----- Price ------+");
-            for(int i = 0; i < getMenuItems.length; i++){
+            for(int i = 0; i < menuItems.length; i++){
                 if(orders[i] > 0){
-                    System.out.printf("| %s\t\t\t\t| \t%6d  | \t\t%10.2f |\n",getMenuItems[i], orders[i],getPrices[i]*orders[i]);
+                    System.out.printf("| %s\t\t\t\t| \t%6d  | \t\t%10.2f |\n", menuItems[i], orders[i], prices[i]*orders[i]);
                 }
             }
             System.out.println("+-------------------+-----------+------------------+");
-            for(int k = 0; k < getMenuItems.length; k++){
-                total += getPrices[k]*orders[k];
+            for(int k = 0; k < menuItems.length; k++){
+                total += prices[k]*orders[k];
             }
             System.out.printf("| Total                         |\t\t%10.2f |\n",total);
 
@@ -45,19 +43,18 @@ public class Skerestaurant {
         return scan.nextLine();
     }
 
-
     public static void showMenu(){
         RestaurantManager readMenu = new RestaurantManager();
-        getMenuItems = readMenu.readFileName();
-        getPrices = readMenu.readFilePrice();
+        menuItems = readMenu.getMenuItems();
+        prices = readMenu.getPrices();
         System.out.println("--------- Welcome to SKE Restaurant ---------");
-        for(int i = 0; i < getMenuItems.length; i++){
-            System.out.printf("[%d] %s\t%10.2f Baht.\n",i+1,getMenuItems[i],getPrices[i]);
+        for(int i = 0; i < menuItems.length; i++){
+            System.out.printf("[%d] %s\t%10.2f Baht.\n",i+1, menuItems[i], prices[i]);
         }
         System.out.println("[t] Total");
         System.out.println("[x] Exit");
         System.out.println();
-        orders = new int[getMenuItems.length];
+        orders = new int[menuItems.length];
 
     }
 
@@ -67,7 +64,7 @@ public class Skerestaurant {
         do{
             choice = getStringReply("\nEnter your choice: ");
             if(isNumber(choice)){
-                if(Integer.parseInt(choice) <= getMenuItems.length){
+                if(Integer.parseInt(choice) <= menuItems.length){
                     quantity = Integer.parseInt(getStringReply("Enter Quantity: "));
                     orders[Integer.parseInt(choice) - 1] += quantity;
                 }
